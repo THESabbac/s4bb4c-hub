c171c7fbc5d4b8bf2612aaf1ba7d5a28f2094892
THESabbac <ofredericofernandes@gmail.com>
2025-04-10 20:22:04 -0300

    s4bb4chub.lua
    0911

--- s4bb4chub.lua
+++ s4bb4chub.lua
@@ -76,7 +76,7 @@ ScriptsPage.ScrollBarImageColor3 = Color3.fromRGB(100,100,100)
 
 -- Botões de scripts unificados
 CreateScriptButton(ScriptsPage, "TCP", "https://raw.githubusercontent.com/OhhMyGehlee/y/refs/heads/main/hj")
-CreateScriptButton(ScriptsPage, "cridadotj", "https://raw.githubusercontent.com/ZhangJunZ84/twvz/refs/heads/main/arisecrossover.lua")
+CreateScriptButton(ScriptsPage, "D", "https://raw.githubusercontent.com/ZhangJunZ84/twvz/refs/heads/main/arisecrossover.lua")
 CreateScriptButton(ScriptsPage, "the fred", "https://raw.githubusercontent.com/ZhangJunZ84/twvz/refs/heads/main/arisecrossover.lua")
 CreateScriptButton(ScriptsPage, "Script x3", "https://raw.githubusercontent.com/ZhangJunZ84/twvz/refs/heads/main/arisecrossover.lua")
 CreateScriptButton(ScriptsPage, "Script x3", "https://raw.githubusercontent.com/ZhangJunZ84/twvz/refs/heads/main/arisecrossover.lua")
